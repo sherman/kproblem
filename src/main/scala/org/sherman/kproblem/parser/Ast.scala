cd2cb6d2bbd9577b3bca0395c9c860d08b738296
Denis M. Gabaidulin <gabaden@gmail.com>
2011-05-24 13:37:47 +0400

    * cosmetics; cleanup

--- src/main/scala/org/sherman/kproblem/parser/Ast.scala
+++ src/main/scala/org/sherman/kproblem/parser/Ast.scala
@@ -1,30 +1,30 @@
 package org.sherman.kproblem.parser
 
-    sealed abstract class Expression {
-        def eval():Int
-    }
+sealed abstract class Expression {
+    def eval(): Int
+}
 
-    case class ExpressionConst(value:Int) extends Expression {
-        def eval():Int = value
-    }
+case class ExpressionConst(value: Int) extends Expression {
+    def eval(): Int = value
+}
 
-    case class ExpressionAdd(a:Expression, b:Expression) extends Expression {
-        def eval():Int = a.eval + b.eval
-    }
-    
-    case class ExpressionSub(a:Expression, b:Expression) extends Expression {
-        def eval():Int = a.eval - b.eval
-    }
-    
-    case class ExpressionMul(a:Expression, b:Expression) extends Expression {
-        def eval():Int = a.eval * b.eval
-    }
-    
-    case class ExpressionDiv(a:Expression, b:Expression) extends Expression {
-        def eval():Int = a.eval / b.eval
-    }
-    
-    case class ExpressionString(a:String) {
-        def eval():String = a
-    }
+case class ExpressionAdd(a: Expression, b: Expression) extends Expression {
+    def eval(): Int = a.eval + b.eval
+}
+
+case class ExpressionSub(a: Expression, b: Expression) extends Expression {
+    def eval(): Int = a.eval - b.eval
+}
+
+case class ExpressionMul(a: Expression, b: Expression) extends Expression {
+    def eval(): Int = a.eval * b.eval
+}
+
+case class ExpressionDiv(a: Expression, b: Expression) extends Expression {
+    def eval(): Int = a.eval / b.eval
+}
+
+case class ExpressionString(a: String) {
+    def eval(): String = a
+}
     
